c5aafc69372e650f149a0a6bfe0d06b887f1dcfe
avidLearnerInProgress <chiragshah9696@gmail.com>
2020-01-11 15:00:22 +0530

    Added solution:

---
+++ simple_text_editor.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int N;
+    cin>>N;
+    stack<string>stk;
+    string st;
+    while(N--){
+        int n; cin>>n;
+        if(n==1){
+            string str;
+            cin>>str;
+            stk.push(st); 
+            st+=str;
+        }
+        else if(n==2){
+            int k;cin>>k;
+             stk.push(st);
+            st.erase(st.size()-k);
+        }
+        else if(n==3){
+            int k;cin>>k;
+            cout<<st[k-1]<<endl; 
+        }
+        else{
+            st = stk.top();  
+            stk.pop();
+        }
+    }
+    return 0;
+}
