8338a004bb9ab0691dade6de89150cede80a2f96
avidLearnerInProgress <chiragshah9696@gmail.com>
2020-01-18 13:26:06 +0530

    Added solution

--- symmetric_tree.cpp
+++ symmetric_tree.cpp
@@ -30,3 +30,25 @@ public:
         return helper(root->left, root->right);
     }
 };
+
+/*
+def isSymmetric(self, root: TreeNode) -> bool:
+        
+        if not root:
+            return True
+    
+        stack = [(root.left, root.right)]
+        
+        while stack:
+            c = stack.pop()
+            l, r = c[0], c[1]
+            
+            if not l and not r:
+                continue
+            if (l and not r) or (r and not l) or (l.val != r.val):
+                return False
+            stack.append((l.right, r.left))
+            stack.append((l.left, r.right))
+        return True
+        
+*/
