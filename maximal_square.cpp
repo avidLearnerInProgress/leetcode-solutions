f0b7f0099c247819c5e5d58553abd026fd8a9e5d
avidLearnerInProgress <chiragshah9696@gmail.com>
2020-02-09 14:11:46 +0530

    Added solution

--- maximal_square.cpp
+++ maximal_square.cpp
@@ -41,3 +41,20 @@ public:
         //return 0;
     }
 };
+
+
+/*
+if(matrix.size()==0 || matrix[0].size()==0)return 0;
+        int maxi=0,n=matrix.size(),m=matrix[0].size();
+        vector<vector<int> > dp(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || j==0)dp[i][j]=matrix[i][j]-'0';
+                else{
+                    if(matrix[i][j]=='1')dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                }
+                maxi=max(maxi,dp[i][j]);
+            }
+        }
+        return maxi*maxi;
+*/
