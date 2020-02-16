b011c7458d4fd9d2ef37ea8c250613f9debf4fde
avidLearnerInProgress <chiragshah9696@gmail.com>
2020-02-17 02:47:30 +0530

    Added solution

---
+++ longest_common_sequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        best = 0
+        for x in nums:
+            if x - 1 not in nums:
+                y = x + 1
+                while y in nums:
+                    y += 1
+                best = max(best, y - x)
+        return best
