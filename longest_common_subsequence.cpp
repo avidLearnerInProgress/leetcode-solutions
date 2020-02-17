74f266d7298247bc5d72caf9ff30380dcb396be2
avidLearnerInProgress <chiragshah9696@gmail.com>
2020-02-17 15:11:38 +0530

    Added solution

---
+++ longest_common_subsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        
+        int m = text1.size(), n = text2.size();
+        
+        if(m == 0 or n == 0) return 0;
+        
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+        for(int i=0; i<=m; i++){
+            for(int j=0; j<=n; j++){
+                if(i==0) dp[i][j] = 0;
+                else if(j==0) dp[i][j] = 0;
+                
+                else{
+                    if(text1[i-1] == text2[j-1])
+                        dp[i][j] = dp[i-1][j-1] + 1;
+                    else
+                        dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
