8984deabdeafb33d8597b7536f56a1ff6ed756ba
avidLearnerInProgress <chiragshah9696@gmail.com>
2020-02-18 23:30:19 +0530

    Added solution

--- minimum_path_sum.cpp
+++ minimum_path_sum.cpp
@@ -1,4 +1,28 @@
-
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        
+        vector<vector<int>> dp(grid.size(), vector<int> (grid[0].size()));
+        
+        for(auto i=0; i<grid.size(); i++){
+            for(auto j=0; j<grid[0].size(); j++){
+                
+                dp[i][j] += grid[i][j];
+                
+                if(i > 0 && j >0)
+                    dp[i][j] += min(dp[i-1][j], dp[i][j-1]);
+                
+                else if(i > 0)
+                    dp[i][j] += dp[i-1][j];
+                
+                else if(j > 0)
+                    dp[i][j] += dp[i][j-1];
+    
+            }
+        }
+        return dp[grid.size()-1][grid[0].size()-1];
+    }
+};
 /*
  int minPathSum(vector<vector<int>>& grid) {
         // DP problem
