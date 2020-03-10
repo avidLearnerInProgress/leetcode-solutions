66bbe3acc43b66afa257f9ee08e9dc1d9678eb84
avidLearnerInProgress <chiragshah9696@gmail.com>
2020-03-10 11:41:55 +0000

    Added solutions

---
+++ odd_even_linked_list.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head) return NULL;
+        
+        auto odd = head, even_head = odd->next, even = even_head;
+        
+        while(even && even->next){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = even_head;
+        return head;
+    }
+};/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head) return NULL;
+        
+        auto odd = head, even_head = odd->next, even = even_head;
+        
+        while(even && even->next){
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = even_head;
+        return head;
+    }
+};
