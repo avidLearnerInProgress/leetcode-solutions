9698af47d1efc66916bdf885da224d32d017a0c3
avidLearnerInProgress <chiragshah9696@gmail.com>
2020-04-25 14:06:03 +0100

    Added min_absolute_difference

--- min_absolute_difference.cpp
+++ min_absolute_difference.cpp
@@ -1,25 +1,20 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-
 class Solution {
 public:
-    int min_ = INT_MAX, tc = -1;
-    
-    int getMinimumDifference(TreeNode* root) {
-        if(!root) return 0;
-        if(root->left)
-            getMinimumDifference(root->left);
-        if(tc>=0) min_ = min(min_, root->val - tc);
-        tc = root->val;
-        if(root->right)
-            getMinimumDifference(root->right);
-        return min_;
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        
+        int min_absolute_difference = INT_MAX;
+        for(int i=0; i<arr.size() - 1; i++){
+            min_absolute_difference = min(abs(arr[i+1] - arr[i]), min_absolute_difference);      
+        }
+        
+        vector<vector<int>> varr;
+        int curr_min = 0;
+        for(int i=0; i<arr.size() - 1; i++){
+            curr_min = abs(arr[i+1] - arr[i]);
+            if(curr_min == min_absolute_difference)
+                varr.push_back({arr[i], arr[i+1]});
+        }  
+        return varr;
     }
 };
