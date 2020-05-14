4a1163a94b2868f93c1bc00eed8a1b7e461a61db
avidLearnerInProgress <chiragshah9696@gmail.com>
2020-05-14 12:44:17 +0100

    Added files

--- 30-days-of-code-may/11.singleElementInSortedArray.cpp
+++ 30-days-of-code-may/11.singleElementInSortedArray.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         
-        int start = 0, end = nums.size() - 1, mid;
-        
+        int start = 0, end = nums.size() - 1, mid;      
         while(start < end){
             mid = start + (end - start) / 2;
-            
             if(mid % 2 == 0){
                 if(nums[mid] == nums[mid+1]) start = mid + 2;
                 else if(nums[mid] == nums[mid-1]) end = mid - 2;
