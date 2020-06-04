9841988fb5a1e03ba834f73af14791400fe6ab1c
Chirag Shah <chiragshah9696@gmail.com>
2020-06-04 16:24:30 +0100

    Create 04.reverseString.cpp

---
+++ 30-days-of-june/04.reverseString.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        if(s.size() == 0) return;
+        
+        int i=0, j = s.size()-1;
+        
+        while(i<=j){
+            swap(s[i], s[j]);
+            i++; j--;
+        }
+        
+        for(auto c : s) cout<<c<<"\t";
+    }
+};
