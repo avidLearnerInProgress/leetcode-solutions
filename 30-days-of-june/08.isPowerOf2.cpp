33475c1da8db0a78db9ab1e1e3123227144e3e93
Chirag Shah <chiragshah9696@gmail.com>
2020-06-08 09:27:19 +0100

    Added day08 solution

---
+++ 30-days-of-june/08.isPowerOf2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 0)  
+        return 0;  
+        while (n != 1)  
+        {  
+            if (n%2 != 0)  
+                return 0;  
+            n = n/2;  
+        }  
+        return 1; 
+    }
+};
