fbe1409015b996b28f9f3b4ab3ae9e9472fc23b3
Chirag Shah <chiragshah9696@gmail.com>
2020-06-21 15:59:28 +0100

    Updated repository

--- 30-days-of-code-may/28.countingBits.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    vector<int> countBits(int num) {
-        vector<int> bits(num+1, 0);
-        for(int i = 1; i <= num; i++) bits[i] += bits[i>>1] + i&1;
-        return bits;   
-    }
-};
