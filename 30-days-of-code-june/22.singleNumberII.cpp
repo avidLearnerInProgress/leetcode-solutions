db5a993267f88a5d7df66b4b37cb34e96033cc7a
Chirag Shah <chiragshah9696@gmail.com>
2020-06-24 18:35:58 +0100

    Added day22 solution

---
+++ 30-days-of-code-june/22.singleNumberII.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+         
+        int n=nums.size();
+        long ans;
+        int x, t;
+        ans=0;
+        int p=0;
+		// We have 32 bits integers as input
+        for(int i=0;i<32;i++)
+        {
+            t=0;
+            //calculate sum of ith bit for all numbers in nums
+            for(int j=0;j<n;j++)
+            {
+                x=nums[j]&1;
+                t=t+x;
+                nums[j]=nums[j]>>1;
+            }
+            t=t%3;
+            //the bit that does not occur as multiple of 3 is left as a remainder 
+            ans=ans+t*pow(2,p);
+            p++;
+        }
+        return ans;
+    }
+};
