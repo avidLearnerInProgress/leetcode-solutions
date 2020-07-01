a1b1be30e0315e8dcb69ac3cabb730d5349bca0c
Chirag Shah <chiragshah9696@gmail.com>
2020-07-01 09:45:11 +0100

    Updated day28 solution

--- 30-days-of-code-june/28.reconstructItinerary.cpp
+++ 30-days-of-code-june/28.reconstructItinerary.cpp
@@ -1,29 +1,24 @@
 class Solution {
-public:
-     vector<string> findItinerary(vector<pair<string, string>> tickets) {
-        
-        // Step 1: Store directed edges in a hash map
-        unordered_map<string, multiset<string>> m;
-        for (const pair<string, string> & ticket : tickets) {
-            m[ticket.first].insert(ticket.second);
-        }
-        
-        // Step 2: March greedily and traceback
-        vector<string> march = { "JFK" }; // the storage for greedy searching
-        vector<string> results; // store the final results reversely
-        while (march.empty() == false) {
-            string & from = march.back();
-            if ((m.find(from) != m.end()) && (m[from].empty() == false)) { // march further
-                multiset<string> & to = m[from];
-                march.push_back(*(to.begin()));
-                to.erase(to.begin());
-            } else { // can not march further, trace back
-                results.push_back(march.back()); // archive the last place
-                march.pop_back();
-            }
-        }
-        reverse(results.begin(), results.end()); // reverse the entries back
-        return results;
+//BEST1: post-order traversal + ordered set
+public: 
+    // Time/Space: O(NlogN), O(N)
+    vector<string> findItinerary(vector<vector<string> >& tickets) {
+        vector<string> res;
+        unordered_map<string, multiset<string> > m;  //this is how to represent the graph        
+        for (auto& a : tickets)  m[a[0]].insert(a[1]);
+        string start("JFK");
+        dfs(m, start, res);  //post-order traversal
+        //return vector<string> (res.rbegin(), res.rend()); // ok
+        return {res.rbegin(), res.rend()}; // better
     }
-};
+    
+    void dfs(unordered_map<string, multiset<string> > &m, string& s, vector<string> &res) {    
+        while (m[s].size()) {
+            auto cur = m[s].begin();
+            string t = *cur;
+            m[s].erase(cur);  //cur ticket is used, so need to be removed
+            dfs(m, t, res);
+        }
+        res.emplace_back(s); // since the root info is consumed in the last, so this is post-order traversal
+    }
 };
