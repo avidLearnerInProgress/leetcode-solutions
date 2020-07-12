04d31f441de94844c35c923776c17e8db4538073
Chirag Shah <chiragshah9696@gmail.com>
2020-07-12 15:01:00 +0100

    Added day10 solution

---
+++ 30-days-of-code-july/10.flattenMultilevelDLL.cpp
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        for(auto h = head; h; h = h->next)
+	    {
+		    if(h->child)
+		    {
+			    Node* next = h->next;
+                h->next = h->child;
+                h->next->prev = h;
+                h->child = NULL;ac
+                
+                auto p = h->next;
+                while(p->next) 
+                    p = p->next;
+                p->next = next;
+                if(next) next->prev = p;
+		}
+	}
+	return head;   
+    }
+};
