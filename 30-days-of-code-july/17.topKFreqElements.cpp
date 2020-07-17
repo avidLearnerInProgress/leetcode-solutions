8d77d69fbb7ee9b5b43ccb5a9cef040e7c6c64b4
Chirag Shah <chiragshah9696@gmail.com>
2020-07-17 12:50:22 +0100

    Added day17 solution

---
+++ 30-days-of-code-july/17.topKFreqElements.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        unordered_map<int, int> cnt;
+        for (auto num : nums) cnt[num]++;
+        for (auto kv : cnt) {
+            pq.push({kv.second, kv.first});
+            while (pq.size() > k) pq.pop();
+        }
+        vector<int> res;
+        while (!pq.empty()) {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        return res;
+    }
+};    
