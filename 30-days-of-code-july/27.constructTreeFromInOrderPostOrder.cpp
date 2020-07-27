6bd54dcade495a5cc35f84094f68c263330ba5d8
Chirag Shah <chiragshah9696@gmail.com>
2020-07-27 20:26:23 +0100

    Added day27 solution

---
+++ 30-days-of-code-july/27.constructTreeFromInOrderPostOrder.cpp
@@ -0,0 +1,12 @@
+TreeNode *Tree(vector<int>& in, int x, int y,vector<int>& po,int a,int b){
+        if(x > y || a > b)return nullptr;
+        TreeNode *node = new TreeNode(po[b]);
+        int SI = x;  
+        while(node->val != in[SI])SI++;
+        node->left  = Tree(in,x,SI-1,po,a,a+SI-x-1);
+        node->right = Tree(in,SI+1,y,po,a+SI-x,b-1);
+        return node;
+    }
+    TreeNode* buildTree(vector<int>& in, vector<int>& po){
+        return Tree(in,0,in.size()-1,po,0,po.size()-1);
+    }
