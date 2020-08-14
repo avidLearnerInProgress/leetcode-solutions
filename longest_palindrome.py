7829d49268f2d421b7d4a2dc2bb60ce8eb39f4fb
Chirag Shah <chiragshah9696@gmail.com>
2020-08-14 19:08:51 +0100

    Update longest_palindrome.py

--- longest_palindrome.py
+++ longest_palindrome.py
@@ -1,14 +1,29 @@
-from collections import Counter
-class Solution:
-    def longestPalindrome(self, s: str) -> int:
-        hashed = dict(Counter(s))
-        sz, szz = 1, len(s)
-        for ele in hashed.values():
-            if ele % 2 == 0:
-                sz +=  (ele -1)
+# from collections import Counter
+# class Solution:
+#     def longestPalindrome(self, s: str) -> int:
+#         hashed = dict(Counter(s))
+#         sz, szz = 1, len(s)
+#         for ele in hashed.values():
+#             if ele % 2 == 0:
+#                 sz +=  (ele -1)
+#             else:
+#                 sz += ele
+#         if sz > szz:
+#             return szz
+#         else:
+#             return sz
+class Solution(object):
+    from collections import Counter
+    def longestPalindrome(self, s):
+        m = set()
+        count = 0
+        for c in s:
+            if c in m:
+                m.remove(c)
+                count += 1
             else:
-                sz += ele
-        if sz > szz:
-            return szz
-        else:
-            return sz
+                m.add(c)
+        return count *  2 + 1 if len(m) != 0 else count * 2
+       
+            
+            
