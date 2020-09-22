9aefe94be75a019df4a6a50840751788f0bd56e7
Chirag Shah <chiragshah9696@gmail.com>
2020-09-22 22:51:47 +0100

    Update trapping_rain_water.cpp

--- trapping_rain_water.cpp
+++ trapping_rain_water.cpp
@@ -1,48 +1,27 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        if(height.size() == 0) return 0;
         
-        vector<int> maxLeft, maxRight;
-
-        //fill maxLeft
-        int maxL = height[0];
-        for(auto h : height){
-            if(h > maxL)
-                maxLeft.push_back(h);
-            else
-                maxLeft.push_back(maxL);
+        if(height.size() == 0) return 0;
+        int n = height.size();
+        int leftMax[n], rightMax[n];
+        
+        leftMax[0] = height[0];
+        rightMax[n-1] = height[n-1];
+        
+        for(int i=1; i<n; i++) {
+            leftMax[i] = max(height[i], leftMax[i-1]);
         }
-
-        //fill maxRight
-        int maxR = height[height.size()-1];
-        for(auto h : height){
-            if(h > maxL)
-                maxRight.push_back(h);
-            else
-                maxRight.push_back(maxR);
+        
+        for(int i=n-2; i>=0; i--) {
+            rightMax[i] = max(height[i], rightMax[i+1]);
         }
-
-        //display maxLeft and maxRight
-        //for(auto element : maxLeft) cout<<element<<"\t";
-        //cout<<"\n";
-        //for(auto element : maxRight) cout<<element<<"\t";
-
-        vector<int> minOfLR;
-        for(auto i=0; i<maxLeft.size(); i++)
-            minOfLR.push_back(min(maxLeft[i], maxRight[i]));
-
-        vector<int> water;
-        for(auto i=0; i<height.size(); i++)
-            water.push_back(minOfLR[i] - height[i]);
-
-        auto total_trapped_water = 0;
-        //cout<<"\n";
-        for(auto w : water) total_trapped_water += w;
-        //cout<<total_trapped_water;
-
-            return total_trapped_water;
+        
+        int totalWater = 0;
+        for(int i=0; i<n; i++) {
+            totalWater += min(leftMax[i], rightMax[i]) - height[i];
+        }
+        
+        return totalWater;
     }
 };
-
-
