ffa37c9227b4b819bb5824f455fe3fd7a7d75c9d
Chirag Shah <chiragshah9696@gmail.com>
2020-11-05 15:46:15 +0000

    Update excel_sheet_column_number.cpp

--- excel_sheet_column_number.cpp
+++ excel_sheet_column_number.cpp
@@ -2,8 +2,12 @@ class Solution {
 public:
     int titleToNumber(string s) {
         int result = 0;
-        for(auto c : s) 
-            result = result * 26 + (c - 'A' + 1);
+        
+        for(auto c : s) {
+            int decimal = c - 'A' + 1; //convert 0 based indexing to 1 based indexing hence added once
+            result = result * 26 + decimal;
+        }
+        
         return result;
     }
-};     
+};
