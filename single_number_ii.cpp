a837434106691f08c5b72384800ff7f4d6d1239f
Chirag Shah <chiragshah9696@gmail.com>
2020-11-30 16:05:16 +0000

    Update single_number_ii.cpp

--- single_number_ii.cpp
+++ single_number_ii.cpp
@@ -1,13 +1,47 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ones = 0;
-        int twos = 0;
-        
-        for( const auto num : nums ){
-            ones = ones^num & ~twos; // add num to the set of ones if num is not in the set of twos
-            twos = twos^num & ~ones; // add num to the set of twos if num is not in the set of ones
+        int ones = 0, twos = 0;
+        for(auto num : nums) {
+            ones = ones ^ num & ~twos; // if num not in twos, add it to ones
+            twos = twos ^ num & ~ones; // if num not in ones, add it to twos
         }
         return ones;
     }
-};	
+};
+
+
+/*
+Previous attempt -
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+         
+        int n=nums.size();
+        long ans;
+        int x, t;
+        ans=0;
+        int p=0;
+		// We have 32 bits integers as input
+        for(int i=0;i<32;i++)
+        {
+            t=0;
+            //calculate sum of ith bit for all numbers in nums
+            for(int j=0;j<n;j++)
+            {
+                x=nums[j]&1;
+                t=t+x;
+                nums[j]=nums[j]>>1;
+            }
+            t=t%3;
+            //the bit that does not occur as multiple of 3 is left as a remainder 
+            ans=ans+t*pow(2,p);
+            p++;
+        }
+        return ans;
+    }
+};
+
+
+*/
