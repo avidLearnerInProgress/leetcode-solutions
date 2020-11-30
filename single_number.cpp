6411029ce49d41ee05df4a61277ef0ba846373a1
Chirag Shah <chiragshah9696@gmail.com>
2020-11-30 15:59:28 +0000

    Update single_number.cpp

--- single_number.cpp
+++ single_number.cpp
@@ -1,5 +1,7 @@
 class Solution:
-    int singleNumber(vector<int> &nums):
+    int singleNumber(vector<int> &nums){
         int x = 0;
         for(auto num : nums) x ^= num;
         return x;
+    }
+};
