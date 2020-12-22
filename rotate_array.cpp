7eb7203735f672c664f201c08a1240465d271dc2
Chirag Shah <chiragshah9696@gmail.com>
2020-12-22 09:35:44 +0000

    Create rotate_array.cpp

---
+++ rotate_array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+        reverse(nums.begin(), nums.begin()+n-k);
+        reverse(nums.begin()+n-k, nums.end());
+        reverse(begin(nums), end(nums));
+    }
+};
