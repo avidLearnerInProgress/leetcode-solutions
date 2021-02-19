5b85629640f645854e27d12d6dd9f76310061e2f
Chirag Shah <chiragshah9696@gmail.com>
2021-02-19 18:02:16 +0000

    Create swap_adjacent_in_LR_string.py

---
+++ swap_adjacent_in_LR_string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canTransform(self, start: str, end: str) -> bool:
+        if len(start) != len(end): return False
+        i, j, l = 0, 0, len(start)
+        while True:
+            while i < l and start[i] == 'X': i += 1
+            while j < l and end[j] == 'X': j += 1
+            if i == l or j == l:
+                if i == j: return True
+                return False
+            if start[i] != end[j]: return False
+            if start[i] == 'L': 
+                if i < j: return False
+            #start[i] == 'R'
+            else:
+                if i > j: return False
+            i, j = i+1, j+1def canTransform(self, start: str, end: str) -> bool:
+        
