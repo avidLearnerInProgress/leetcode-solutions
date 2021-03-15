bd8583abaee60b20de218a83336a5405046b8f76
Chirag Shah <chiragshah9696@gmail.com>
2021-03-15 22:52:09 +0000

    Update max_chunks_to_make_sorted_ii.cpp

--- max_chunks_to_make_sorted_ii.cpp
+++ max_chunks_to_make_sorted_ii.cpp
@@ -10,10 +10,12 @@ public:
         
         int N = arr.size();
         vector<int> trackRightMin(N);
-        int result = 0, max_ = 0;
+        int result = 0, max_ = INT_MIN;
         
+        
+        trackRightMin[N-1] = arr[N-1];
         for(int i=N-2; i>=0; --i) 
-            trackRightMin[i] = min(arr[i+1], arr[i]);
+            trackRightMin[i] = min(trackRightMin[i+1], arr[i]);
         
         for(int i=0; i<N-1; ++i) {
             max_ = max(max_, arr[i]);
