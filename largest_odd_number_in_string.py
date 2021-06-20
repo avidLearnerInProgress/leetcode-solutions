24ae847c9f7bdee40e7368db6bdc912ec16491a5
Chirag Shah <chiragshah9696@gmail.com>
2021-06-20 10:03:34 +0100

    Create largest_odd_number_in_string.py

---
+++ largest_odd_number_in_string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        
+        for i in range(len(num) - 1, -1, -1):
+            if int(num[i]) % 2 == 1:
+                return num[:i+1]
+        return ""
