f025329904b146a45c756fb64cbe7c60c474e68c
Chirag Shah <chiragshah9696@gmail.com>
2021-06-21 20:31:31 +0100

    Update container_with_max_water.py

--- container_with_max_water.py
+++ container_with_max_water.py
@@ -1,13 +1,32 @@
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#         #move left pointer or right pointer only when height is smaller
+#         i, j = 0, len(height) - 1
+#         capacity = 0
+#         while i < j:
+#             #width of container * min_height
+#             capacity = max(capacity, (j-i) * min(height[i], height[j]))
+#             if height[i] < height[j]:
+#                 i+=1
+#             else:
+#                 j-=1
+#         return capacity
+
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        #move left pointer or right pointer only when height is smaller
-        i, j = 0, len(height) - 1
-        capacity = 0
-        while i < j:
-            #width of container * min_height
-            capacity = max(capacity, (j-i) * min(height[i], height[j]))
-            if height[i] < height[j]:
-                i+=1
+        left, right = 0, len(height) - 1
+        maxWaterCapacity = 0
+        
+        while left <= right:
+            currentWaterCapacity = (right - left) * min(height[left], height[right])
+            if maxWaterCapacity < currentWaterCapacity:
+                maxWaterCapacity = currentWaterCapacity
+            
+            if height[left] <= height[right]:
+                left += 1
             else:
-                j-=1
-        return capacity
+                right -= 1
+                
+        return maxWaterCapacity
+            
