2c7e6390324635bdf99d5d71ebc3bc55cde35572
Chirag Shah <chiragshah9696@gmail.com>
2021-06-22 21:41:27 +0100

    Update find_duplicate_number.cpp

--- find_duplicate_number.cpp
+++ find_duplicate_number.cpp
@@ -1,13 +1,42 @@
+// Non - optimal solution:
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+        
+//         sort(nums.begin(), nums.end());
+        
+//         for(auto i = nums.begin(); i != nums.end()-1; i++){
+//             if(*(i) == *(i+1))
+//                 return *(i);
+//         }
+//         return -1;
+//     }
+// };
+
+// Optimal solution - O(n) || O(1)
 class Solution {
 public:
+    
+    // Very similar to find the point where there is a loop in the linked list
+    // Treat array as a circular list with the duplicate element as the intersection point
+    // Hair and Tortoise method
     int findDuplicate(vector<int>& nums) {
+        if(nums.size() <= 1) return -1;
         
-        sort(nums.begin(), nums.end());
+        int fast = 0, slow = 0;
         
-        for(auto i = nums.begin(); i != nums.end()-1; i++){
-            if(*(i) == *(i+1))
-                return *(i);
+        while(true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]]; // take index as array element 
+            if(slow == fast) break;
         }
-        return -1;
+        
+        fast = 0;
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
     }
 };
