d25dcd475b9a7232336e606fe243c3fdd98bf8a2
Chirag Shah <chiragshah9696@gmail.com>
2021-07-22 20:52:55 +0100

    Update deepest_sum_leaves.cpp

--- deepest_sum_leaves.cpp
+++ deepest_sum_leaves.cpp
@@ -1,35 +1,72 @@
+```cpp
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int maxHeight(TreeNode* root){
-        if(root == NULL)
-            return 0;
-        return 1 + max(maxHeight(root->left), maxHeight(root->right));
-    }
-    int sum = 0;
     
-    int _dLS(TreeNode* root, int height, int level){
-        if(!root) return 0;
-        if(root->left==NULL and root->right==NULL and level == height){
-            sum += root->val;
+    //recursive
+    void helper(TreeNode *root, int current, int &maxLevel, int &sum) {
+        if(!root) return;
+        helper(root->left, current + 1, maxLevel, sum);
+        
+        if(!root->left and !root->right) {
+            if(current > maxLevel) {
+                maxLevel = current;
+                sum = 0; //smart
+            }
+            if(current == maxLevel) {
+                sum += root->val;
+            }
         }
-        _dLS(root->left, height, level + 1);
-        _dLS(root->right, height, level + 1);
-        return sum;
+      
+        helper(root->right, current + 1, maxLevel, sum); 
     }
     
     int deepestLeavesSum(TreeNode* root) {
-        if(root == NULL) return 0;
-        int height = maxHeight(root);
-        int level = 1;
-        return _dLS(root, height, level);
+        if(!root) return 0;
+        
+        int maxLevel = 0;
+        int currentLevel = 0, sum = 0;
+        helper(root, currentLevel, maxLevel, sum);    
+        
+        return sum;
     }
+    
+    
+    //iterative
+    int deepestLeavesSum(TreeNode* root) {
+        if(!root) return 0;
+        
+        queue<TreeNode *> qu;
+        TreeNode *current = root;
+        qu.push(current);
+        
+        int sum = 0;
+        
+        while(!qu.empty()) {
+            int n = qu.size();
+            sum = 0;
+            for(int i = 0; i < n; i++) {
+                
+                current = qu.front();
+                qu.pop();
+                
+                sum += current->val;
+                if(current->left) qu.push(current->left);
+                if(current->right) qu.push(current->right);
+            }
+        }
+        return sum;
+    }
+    
 };
+```
