5cc26758ab4980d54f4e6c829bbbcef1edf1493e
Chirag Shah <chiragshah9696@gmail.com>
2021-07-25 16:16:41 +0100

    Update vertical_order_traversal.cpp

--- vertical_order_traversal.cpp
+++ vertical_order_traversal.cpp
@@ -4,42 +4,81 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        if(!root) return vector<vector<int>>();
+// recursive
+    void horizontalDistanceHelper(TreeNode *root, map<int, vector<pair<int, int>>> &mp, int height, int hd) {
+        if(!root) return;
         
-        //map of horizontal distance and list of Nodes coming within that positive distance
-        map<int, vector<int> > m;
-        int hd = 0; // horizontal distance
+        mp[hd].push_back({height, root->val});
+        
+        horizontalDistanceHelper(root->left, mp, height + 1, hd - 1);
+        horizontalDistanceHelper(root->right, mp, height + 1, hd + 1);
+    }    
+    
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        if(!root) return {};
+        
+        map<int, vector<pair<int, int>> > mp;
+        vector<vector<int>> result;
+        horizontalDistanceHelper(root, mp, 0, 0);
+        int i = 0;
+        
+        for(auto [k,v] : mp) {
+            vector<int> hdResult;
+            
+            sort(v.begin(), v.end(), [](auto a, auto b) {
+                if(a.first == b.first) return a.second < b.second;
+                else return a.first < b.first;
+            });
+                 
+            for(auto &[a, b]: v) {
+                hdResult.push_back(b);
+            }
+            result.push_back(hdResult);
+            hdResult.clear();
+        }
+        return result;  
+    }
+    
+// iterative
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        if(!root) return {};
         vector<vector<int>> result;
         
-        //queue for level order traversal
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, hd});
+        map<int, vector<int>> mp; //map of horizontal distance and list of Nodes coming within that positive distance
+        queue<pair<TreeNode*, int>> qu; //queue to keep track of level. => pair<node, horizontal_distance>
         
-        while(!q.empty()){
-            set<pair<int, int>> sameposnodes;
-            int sz = q.size();
-            while(sz-- > 0) {
-                
-                auto [curr, hd] = q.front(); q.pop();
-                sameposnodes.insert({hd, curr->val});
-                
-                if(curr->left)
-                    q.push({curr->left, hd - 1});
-                if(curr->right)
-                    q.push({curr->right, hd + 1});
-            }
+        qu.push({root, 0});
+        
+        while(!qu.empty()) {
+            int n = qu.size();
+            multiset<pair<int, int>> samelevelnodes;
             
-            for(auto s : sameposnodes) m[s.first].push_back(s.second);
+            while(n-- > 0) {
+                auto [current, hd] = qu.front(); 
+                samelevelnodes.insert({hd, current->val});
+                qu.pop();
+            
+                if(current->left) 
+                    qu.push({current->left, hd - 1});
+                
+                if(current->right) 
+                    qu.push({current->right, hd + 1});
+            }
+            for(auto s : samelevelnodes) {
+                mp[s.first].push_back(s.second);
+            }
         }
         
-        
-        for(auto kv : m)  result.push_back(kv.second);
-        return result;    
+        for(auto kv : mp) {
+            result.push_back(kv.second); //here kv.second is a vector of nodes at the hd(kv.first)
+        }
+        return result;  
     }
 };
