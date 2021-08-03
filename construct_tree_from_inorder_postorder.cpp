329dbd36eb2b48aa96eed8ddad54377d89bbb0a4
Chirag Shah <chiragshah9696@gmail.com>
2021-08-03 22:04:49 +0100

    Update construct_tree_from_inorder_postorder.cpp

--- construct_tree_from_inorder_postorder.cpp
+++ construct_tree_from_inorder_postorder.cpp
@@ -10,27 +10,39 @@
  * };
  */
 class Solution {
-private:
-    unordered_map<int,int> mp;
-    TreeNode* help(vector<int>& in, vector<int>& pos, int left, int right, int rootidx){
-
-        if(left > right)
-            return NULL;
-        int val = pos[rootidx];
-        TreeNode* node = new TreeNode(val);
-        int p = mp[val];
-        node->right = help(in,pos,p+1,right,rootidx-1);
-        node->left = help(in,pos,left,p-1,rootidx-1-(right-p));
-
-        return node;
-    }
 public:
-    TreeNode* buildTree(vector<int>& in, vector<int>& pos) {
-        for(int i=0;i<in.size();i++){
-            mp[in[i]]=i;
+    unordered_map<int, int> inorderMap;
+    TreeNode *buildTreeHelper(vector<int> &postorder, int i, int j, int &postorderIndex) {
+        if(i > j) return NULL;
+        
+        int postorderValue = postorder[postorderIndex]; //get the postorder value to create the new node
+        TreeNode *root = new TreeNode(postorderValue); //create new node
+        
+        int inorderIndex = inorderMap[postorderValue]; //get the index for dividing the inorder array into two parts
+        postorderIndex--; //decrement the preorderIndex for next recursive call
+        
+        //recurse for right subtree first and then the left subtree(since its a postorder traversal)
+        root->right = buildTreeHelper(postorder, inorderIndex + 1, j, postorderIndex);
+        root->left = buildTreeHelper(postorder, i, inorderIndex - 1, postorderIndex); 
+        
+        
+        return root;
+    }
+    
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        
+        if(postorder.size() == 0 or inorder.size() == 0) {
+            return NULL;
         }
-
-        return help(in,pos,0,in.size()-1,pos.size()-1);
+        
+        int idx = 0;
+        for(auto ele : inorder) {
+            inorderMap[ele] = idx++;
+        }
+        
+        int i = 0, j = postorder.size() - 1;
+        int postorderIndex = postorder.size() - 1;
+        
+        return buildTreeHelper(postorder, i, j, postorderIndex);
     }
 };
-
