608c32d7356395a8c6f01295f93f06e52ab0d49e
Chirag Shah <chiragshah9696@gmail.com>
2021-08-03 22:05:33 +0100

    Update construct_tree_from_inorder_preorder.cpp

--- construct_tree_from_inorder_preorder.cpp
+++ construct_tree_from_inorder_preorder.cpp
@@ -11,27 +11,35 @@
  */
 class Solution {
 public:
-    unordered_map<int,int> lookUpForIndex;
-    TreeNode* solve(vector<int>&  preorder,vector<int>& inorder,int i ,int j,int& preorderIndex)
-    {
-        if(i > j || preorderIndex >= preorder.size()) return nullptr;
-
-        int preorderValue =  preorder[preorderIndex];
-        TreeNode* root = new TreeNode(preorderValue);
-
-        int inorderIndex = lookUpForIndex[preorderValue];
-        preorderIndex++;
-
-        root->left = solve(preorder,inorder, i, inorderIndex - 1, preorderIndex);
-        root->right = solve(preorder,inorder, inorderIndex + 1, j, preorderIndex);
+    unordered_map<int, int> inorderMap;
+    TreeNode *buildTreeHelper(vector<int> &preorder, vector<int> &inorder, int i, int j, int &preorderIndex) {
+        if(i > j or preorderIndex > preorder.size()) return NULL;
+        
+        int preorderValue = preorder[preorderIndex]; //get the preorder value to create the new node
+        TreeNode *root = new TreeNode(preorderValue); //create new node
+        
+        int inorderIndex = inorderMap[preorderValue]; //get the index for dividing the inorder array into two parts
+        preorderIndex++; //increment the preorderIndex for next recursive call
+        
+        root->left = buildTreeHelper(preorder, inorder, i, inorderIndex - 1, preorderIndex); //recurse for left and right subtrees
+        root->right = buildTreeHelper(preorder, inorder, inorderIndex + 1, j, preorderIndex);
+        
         return root;
     }
+    
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        for(int i = 0;i < inorder.size();i++)
-            lookUpForIndex[inorder[i]]=i;
-
+        
+        if(preorder.size() == 0 or inorder.size() == 0) {
+            return NULL;
+        }
+        int idx = 0;
+        for(auto ele : inorder) {
+            inorderMap[ele] = idx++;
+        }
+        
+        int i = 0, j = preorder.size() - 1;
         int preorderIndex = 0;
-        return solve(preorder, inorder,  0, preorder.size()-1, preorderIndex);
+        
+        return buildTreeHelper(preorder, inorder, i, j, preorderIndex);
     }
-
 };
