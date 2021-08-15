c6b87cb88d6f3393a8c4b03bed1d120deb75d9d8
Chirag Shah <chiragshah9696@gmail.com>
2021-08-15 23:19:26 +0100

    Update top_k_frequent_words.cpp

--- top_k_frequent_words.cpp
+++ top_k_frequent_words.cpp
@@ -1,4 +1,7 @@
 class Solution {
+    
+    //here we use < because the priority queue in cpp stl is a max heap. so to achieve true functionality or in other words to achieve the case when first element will be greater than the second, we put lhs.first < rhs.first - this means that logically this condition will hold true they will be greater.
+    
     struct Comp {
         bool operator()(const pair<int, string>& lhs, const pair<int, string>& rhs) const {
             if (lhs.first != rhs.first)
@@ -13,10 +16,50 @@ public:
             ++cnt[w];
         
         priority_queue<pair<int, string>, vector<pair<int, string>>, Comp> Q;
-        for (auto it : cnt)
+        vector<pair<int, string>> V;
+        for (auto it : cnt) {
             Q.push({it.second, it.first});
+            V.emplace_back(it.second, it.first);
+        }
+        
+        //using same custom comparator to test on Priority Queue 
+        while(!Q.empty()) {
+            cout<<Q.top().first<<" "<<Q.top().second<<"\n";
+            Q.pop();
+        }
+        cout<<"---- \n\n\n ---\n";
+        
+        //using same custom comparator to test on Vector 
+        sort(V.begin(), V.end(), [](const pair<int, string>& lhs, const pair<int, string>& rhs) {
+            if (lhs.first != rhs.first)
+                return lhs.first < rhs.first;
+            return lhs.second > rhs.second;
+        });
+        
+        for(auto &ele : V) {
+            cout<<ele.first<<" "<<ele.second<<"\n";
+        }
+        
+        /* output =>
+        3 abc
+        2 i
+        2 love
+        1 coding
+        1 leetcode
+        ---- 
+
+
+         ---
+        1 leetcode
+        1 coding
+        2 love
+        2 i
+        3 abc
+        
+        */
         
         vector<string> res;
+        
         while (k-- > 0 && !Q.empty()) {
             res.push_back(Q.top().second);
             Q.pop();
