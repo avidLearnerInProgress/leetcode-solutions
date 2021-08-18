b6a063a097eafc2ae369dae857686c331e3311ff
Chirag Shah <chiragshah9696@gmail.com>
2021-08-18 11:48:07 +0100

    Update sort_char_by_frequency.cpp

--- sort_char_by_frequency.cpp
+++ sort_char_by_frequency.cpp
@@ -37,15 +37,17 @@ class Solution {
     struct Comp {
       bool operator()(const pair<int, int> &l, const pair<int, int> &r) {
           if(l.first != r.first) {
+              // for max heap return true if the l has higher order(priority) than r
               return l.first < r.first;
           }
+          // return true if l has greater lexicographical order(r before t here) than r
           return l.second > r.second;
       }  
     };
 public:
     string frequencySort(string s) {
         if(s.empty()) return s;
-        priority_queue<pair<int, int>, vector<pair<int, int> >, Comp> Q; 
+        priority_queue<pair<int, char>, vector<pair<int, char> >, Comp> Q; 
         unordered_map<int, int> count;
         
         for(const char &c : s) 
@@ -55,13 +57,17 @@ public:
             Q.emplace(v, k);
         }
         
-        string result = "";
         while(!Q.empty()) {
-            int frequency = Q.top().first;
-            while(frequency--)
-                result += Q.top().second;
+            cout<<Q.top().first<<Q.top().second<<"\n";
             Q.pop();
         }
+        string result = "";
+        // while(!Q.empty()) {
+        //     int frequency = Q.top().first;
+        //     while(frequency--)
+        //         result += Q.top().second;
+        //     Q.pop();
+        // }
         return result;
     }
 };
