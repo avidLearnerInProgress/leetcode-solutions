2419513d20c0a25e3c9dbf77d00237b29c4ab2d8
Chirag Shah <chiragshah9696@gmail.com>
2021-08-20 11:54:42 +0100

    Update valid_sudoku.cpp

--- valid_sudoku.cpp
+++ valid_sudoku.cpp
@@ -1,3 +1,82 @@
+
+//more readable, less optimal
+class Solution {
+private:
+    
+    bool isValidRow(vector<vector<char>>& board, int& row) {
+        
+        set<int> digits; // SC: O(1)
+        for(int c = 0; c < 9; c++) {
+            if(digits.find(board[row][c]) != digits.end()) { // O(log(row_size))
+                return false;
+            }
+            if(board[row][c] != '.') {
+                digits.emplace(board[row][c]);
+            }
+        }
+        digits.clear();
+        return true;
+    }
+    
+    bool isValidColumn(vector<vector<char>>& board, int& col) {
+        
+        set<int> digits; // SC: O(1)
+        for(int r = 0; r < 9; r++) {
+            if(digits.find(board[r][col]) != digits.end()) { // O(log(row_size))
+                return false;
+            }
+            if(board[r][col] != '.') {
+                digits.emplace(board[r][col]);
+            }
+        }
+        digits.clear();
+        return true;
+    }
+    
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        
+        //validate all rows
+        for(int r = 0; r < 9; r++) {
+            if(!isValidRow(board, r)) { //O(r)
+                return false;
+            }
+        }
+        
+        //validate all cols
+        for(int c = 0; c < 9; c++) {
+            if(!isValidColumn(board, c)) { //O(r)
+                return false;
+            }
+        }
+        
+       //valid 3x3 blocks
+        set<int> digits;        
+        for(int blockRow = 0; blockRow < 3; blockRow++) {
+            for(int blockCol = 0; blockCol < 3; blockCol++) {
+                
+                for(int r = 3 * blockRow; r < 3 * blockRow + 3; r++) {
+                    for(int c = 3 * blockCol; c < 3 * blockCol + 3; c++) {
+                        
+                        if(digits.find(board[r][c]) != digits.end()) {
+                            return false;
+                        }
+                        if(board[r][c] != '.') {
+                            digits.emplace(board[r][c]);
+                        }
+                    }
+                }
+                digits.clear();
+            }
+        }
+        
+        return true;
+    }
+};
+
+
+
+//less readable, more optimal
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
